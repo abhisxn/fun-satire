7ea24b28d0777126bdea4340582febe5501a1ce3
Kilo <kilo@local>
2026-07-25 23:56:09 +0530

    chore(render): add drawBug barrel export to drawers/index.ts

--- src/render/drawers/index.ts
+++ src/render/drawers/index.ts
@@ -4,3 +4,5 @@ export { drawEye } from "./drawEye";
 export { drawSubject, SUBJECT_DRAW } from "./drawSubject";
 export type { DrawSubjectInput } from "./drawSubject";
 export { computeGazeLines } from "./drawGazeLines";
+export { drawBug, computeScuttleJitter, BUG_DRAW } from "./drawBug";
+export type { DrawBugInput } from "./drawBug";
